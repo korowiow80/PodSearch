4483ea9a1422e53547d442d8552fd7826ff8b1b2
Bengt Lüers <Bengt.Lueers@gmail.com>
2012-05-16 14:36:06 +0200

    Crawler now with generic names and prefixes using tldextract

--- src/Crawler/Crawler/items.py
+++ src/Crawler/Crawler/items.py
@@ -5,7 +5,7 @@
 
 from scrapy.item import Item, Field
 
-class CrawlerItem(Item):
+class PodcastFeedItem(Item):
     # define the fields for your item here like:
     # name = Field()
     title = Field()
