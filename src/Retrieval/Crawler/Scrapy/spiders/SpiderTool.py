bb98fa388c958e3ec1dabed66e3bc61a9f72556b
Bengt Lüers <Bengt.Lueers@gmail.com>
2012-05-23 00:03:02 +0200

    Introducing DownloaderTool and FeedDownloader + necessary modifications

--- src/Retrieval/Crawler/Scrapy/spiders/SpiderTool.py
+++ src/Retrieval/Crawler/Scrapy/spiders/SpiderTool.py
@@ -1,5 +1,3 @@
-#! /bin/python
-
 import os
 import errno
 from urlparse import urlparse
@@ -8,50 +6,64 @@ import tldextract
 
 
 class SpiderTool:
+    
+    projectRoot = "../../../"
 
     def __init__ (self):
         pass
         
-    def derive (self, url):        
-        # extract fullDomain from URL
-        extract = tldextract.extract(url)
-        if extract.subdomain:
-            fullDomain = ".".join(extract)
-            domainTld = ".".join(extract[1:])
-        else:
-            fullDomain = ".".join(extract[1:])
-            domainTld = None
-        
-        # derive name from TLD
-        # by general convention the first letter of a class gets capitalized
-        # by our convention, we skip the sub-domain, if it is 'www'
-        if domainTld:
-            name = domainTld[0].upper() + domainTld.replace('.', '_')[1:]
-        else:
-            name = fullDomain[0].upper() + fullDomain.replace('.', '_')[1:]
-        
-        # derive prefix from domain
-        # by convention we skip the www
-        if domainTld:
-            prefix = "../../../static/0-Directories/" + domainTld + "/"
-            feedListFile = "../../../static/1-Feedlists/" + domainTld + ".json"
-        else:
-            prefix = "../../../static/0-Directories/" + fullDomain + "/"
-            feedListFile = "../../../static/1-Feedlists/" + fullDomain + ".json"
+    def derive (self, url):
+        fullDomain, domain = self.getDomain(url)
+        spiderName = self.getSpiderNameFromUrl(domain)
+        directoryPath = self.getDirectoryPath(domain)
+        feedListFilePath = self.getFeedListFilePath(domain)
+        self.ensurePathExists(directoryPath)        
+        baseUrl = self.getBaseUrl(url)
+        return baseUrl, feedListFilePath, spiderName, directoryPath
 
-        # make sure the prefix exists
+    def makeSurePathExists(self, path):
+        """Makes sure a given path exists.
+        Tries to create the given path, handles eventual failure."""
         try:
-            os.makedirs(prefix)
+            os.makedirs(path)
         except OSError as exc:
             if exc.errno == errno.EEXIST: pass
             else: raise
+        return
 
-        # derive baseUrl from url
-        o = urlparse(url)
-        baseUrl = o.scheme + '://' + o.netloc
-            
-        return baseUrl, feedListFile, name, prefix
-    
-    def getAbsoluteUrl (self, url, baseUrl):
+    def getAbsoluteUrl(self, url, baseUrl):
         if url.startswith('http'): return url
         else: return baseUrl + url
+
+    def getBaseUrl(self, url):
+        """Derives the baseUrl from a given url."""
+        o = urlparse(url)
+        baseUrl = o.scheme + '://' + o.netloc
+        return baseUrl
+
+    def getDomain(self, url):
+        """Extracts the full and the top-level domain from a given URL.
+        By our convention, we skip the sub-domain, if it is 'www'."""
+        extract = tldextract.extract(url)
+        if extract.subdomain and extract.subdomain != 'www':
+            domain = ".".join(extract)
+        else:
+            domain = ".".join(extract[1:])
+        return domain
+
+    def getSpiderNameFromUrl(self, domain):
+        """Derives the spider name from the given domain and fullDomain.
+        By general convention the first letter of a class gets capitalized."""
+        if domain:
+            spiderName = domain[0].upper() + domain.replace('.', '_')[1:]
+        return spiderName
+
+    def getDirectoryPath(self, domain):    
+        """Derives the directory path from domain."""
+        directoryPath = "../../../static/0-Directories/" + domain + "/"
+        return directoryPath
+    
+    def getFeedListFilePath(self, domain):
+        """Derives the feedlist path from a given domain and fullDomain."""
+        feedListFilePath = "../../../static/1-Feedlists/" + domain + ".json"
+        return feedListFilePath
