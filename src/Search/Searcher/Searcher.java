829921bd8061f8bdcdaca4677b80fcb0cd3ffaf1
Mihail Mihalache <mihalache_mihail@yahoo.com>
2012-05-23 11:49:34 +0200

    Searcher ~

--- src/Search/Searcher/Searcher.java
+++ src/Search/Searcher/Searcher.java
@@ -1,4 +1,6 @@
-package Searcher;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 
 import java.io.IOException;
 
@@ -15,47 +17,51 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
 public class Searcher {
-	public static void main(String[] args) throws IllegalArgumentException,
-	IOException, ParseException {
-		if (args.length != 2) {
-			throw new IllegalArgumentException("Usage: java " + Searcher.class.getName()
-					+ " <index dir> <query>");
-			}
-		String indexDir = args[0];					//1
-		String q = args[1];							//2		
-		
+	public static void main(String[] args) throws Exception {
+		String indexDir = "../../static/3-Index";					//1
+		String q = "";  										//2	
+		// Read the query from the console
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+			System.out.print("Search: ");
+			q = in.readLine();			
+		} 
+		catch (IOException e) {
+		}
+
 		search(indexDir, q);
 	}
-	
+
 	public static void search(String indexDir, String q)
 			throws IOException, ParseException {
-		
+
 		Directory dir = FSDirectory.open(new File(indexDir)); 		//3
 		IndexSearcher is = new IndexSearcher(dir);					//3
-		
-		QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, //4
-							"contents", 							 //4
-							new StandardAnalyzer(					 //4						
-							Version.LUCENE_CURRENT)); 				 //4
-		
-				Query query = parser.parse(q);						 //4
-				long start = System.currentTimeMillis();
-				TopDocs hits = is.search(query, 10); 				 //5
-				long end = System.currentTimeMillis();
-				
-				System.err.println("Found " + hits.totalHits +		 //6
-						" document(s) (in " + (end - start) +		 //6
-						" milliseconds) that matched query '" +		 //6
-						q + "':");									 //6
-				
-				for(int i=0;i<hits.scoreDocs.length;i++) {
-						ScoreDoc scoreDoc = hits.scoreDocs[i];		 //7
-						Document doc = is.doc(scoreDoc.doc);		 //7
 
-						System.out.println(doc.get("filename"));	 //8
-				}
-				
-				is.close(); 										 //9	
+		QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, //4
+				"contents", 							 //4
+				new StandardAnalyzer(					 //4						
+						Version.LUCENE_CURRENT)); 				 //4
+
+		Query query = parser.parse(q);						 //4
+		long start = System.currentTimeMillis();
+		TopDocs hits = is.search(query, 10); 				 //5
+		long end = System.currentTimeMillis();
+
+		System.err.println("Found " + hits.totalHits +		 //6
+				" document(s) (in " + (end - start) +		 //6
+				" milliseconds) that matched query '" +		 //6
+				q + "':");									 //6
+
+		for(int i=0;i<hits.scoreDocs.length;i++) {
+			ScoreDoc scoreDoc = hits.scoreDocs[i];		 //7
+			Document doc = is.doc(scoreDoc.doc);		 //7
+
+			System.out.println(doc.get("filename"));	 //8
 		}
+
+		is.close(); 										 //9	
+	}
 }
 
